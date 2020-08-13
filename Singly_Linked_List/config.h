739c18e503b27b63d052985aba03262f4b8ae25b
shivang99 <56412214+shivang99@users.noreply.github.com>
2020-08-13 17:33:28 -0400

    Update config.h

--- Singly_Linked_List/config.h
+++ Singly_Linked_List/config.h
@@ -3,7 +3,7 @@
  * Activate test #N by defining the corresponding LAB5_TESTN to have the value 1.
  */
 
-#define LAB5_TEST1	1	// 1 means test with int instead of char
-#define LAB5_TEST2	0	// Activate moveToBeginning (prog exercise 2)
-#define LAB5_TEST3	0	// Activate insertBefore (prog exercise 3)
+#define TEST1	1	// 1 means test with int instead of char
+#define TEST2	0	// Activate moveToBeginning (prog exercise 2)
+#define TEST3	0	// Activate insertBefore (prog exercise 3)
 
