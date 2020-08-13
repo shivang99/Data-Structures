e5d61eedc878c2a93d4dda727217e10994eb0a78
shivang99 <56412214+shivang99@users.noreply.github.com>
2020-08-13 17:23:15 -0400

    Add files via upload

---
+++ Singly_Linked_List/ListLinked.cpp
@@ -0,0 +1,401 @@
+#include "ListLinked.h"
+#include "iostream"
+//Name: Shivang Trivedi
+//Date: Jan 30, 2019
+//Lab: 05 - Take Home
+template<typename DataType>
+inline List<DataType>::List(int ignored)
+{
+	//initialize to null
+	head = NULL;
+	cursor = NULL;
+}
+
+template<typename DataType>
+List<DataType>::List(const List & other)
+{
+	//create a new listnode type and set it to others 
+	ListNode *otherPtr = other.head;
+	ListNode *holdCursor = 0;
+	//while the other is not zero, insert the new item
+	while (otherPtr != 0) {
+		insert(other->dataItem);
+		if (otherPtr == other.cursor)
+			holdCursor = cursor;
+		otherPtr = otherPtr->next;
+	}
+	//update cursor
+	cursor = holdCursor;
+}
+
+template<typename DataType>
+List<DataType>& List<DataType>::operator=(const List & other)
+{
+	//if they are not the same then copy it
+	if (this != other) {
+		ListNode *otherPtr = other.head;
+		ListNode *holdCursor = 0;
+		//while other pointer is not zero, copy each cursor from other
+		while (otherPtr != 0) {
+			insert(other->dataItem);
+			if (otherPtr == other.cursor)
+				holdCursor = cursor;
+			otherPtr = otherPtr->next;
+		}
+		//update cursor
+		cursor = holdCursor;
+	}
+	return *this;
+}
+
+
+template<typename DataType>
+List<DataType>::~List()
+{
+	//set cursor to head
+	cursor = head;
+	ListNode *x;
+	//delete each node until it equals to 0
+	while (cursor != 0) {
+		x = cursor;
+		cursor = cursor->next;
+		delete x;
+	}
+
+}
+
+template<typename DataType>
+void List<DataType>::insert(const DataType & newDataItem) throw(logic_error)
+{
+	//if the list is empty, set the head to newDataItem
+	if (head == NULL) {
+		head = new ListNode(newDataItem, 0);
+		cursor = head;
+	}
+	else {
+		//insert a new node
+		ListNode *temp;
+		temp = new ListNode(newDataItem, 0);
+		if (cursor->next == 0) {
+			cursor->next = temp;
+			//update cursor
+			cursor = cursor->next;
+		}
+		else {
+			//if the cursor is in the middle of the list, add temp in the middle after the cursor
+			temp->next = cursor->next;
+			cursor->next = temp;
+			cursor = temp;
+		}
+		
+	}
+}
+
+template<typename DataType>
+void List<DataType>::remove() throw(logic_error)
+{
+	ListNode *p, *q;
+	if (head == NULL) {
+		throw logic_error("List is empty\n");
+	}
+	else {
+		//if cursor at the beginning, set p to head and change head and cursor to the next node and delete p
+		if (cursor == head) {
+			p = head; 
+			head = cursor->next;
+			cursor = cursor->next;
+			delete p;
+		}
+		/*if the cursor is at the end, set p to cursor and cursor to head and go through 
+		the list and set q to cursor and point q to NULL */
+		else if (cursor->next == 0) {
+			p = cursor;
+			cursor = head;
+			while (cursor->next != p) {
+				cursor = cursor->next;
+			}
+			q = cursor;
+			q->next = NULL;
+			delete p;
+			cursor = head;
+		}
+		/*if the cursor is somewhere in the middle of the list, set p to cursor and cursor to head
+		go through the list until it reaches the node before p, set q to that and set q pointer to p ponter
+		and cursor to p pointer.*/
+		else {
+			p = cursor;
+			cursor = head;
+			while (cursor->next != p) {
+				cursor = cursor->next;
+			}
+			q = cursor;
+			q->next = p->next;
+			cursor = p->next;
+			delete p;
+		}
+	}
+}
+
+template<typename DataType>
+void List<DataType>::replace(const DataType & newDataItem) throw(logic_error)
+{
+	//check if the list is empty or not
+	if (head == NULL) {
+		throw logic_error("List is empty\n");
+	}
+	else {
+		//replace current item with new item
+		cursor->dataItem = newDataItem;
+	}
+}
+
+template<typename DataType>
+void List<DataType>::clear()
+{
+	//set cursor to head
+	cursor = head;
+	ListNode *x;
+	//delete each node until it equals to 0
+	while (cursor != 0) {
+		x = cursor;
+		cursor = cursor->next;
+		delete x;
+	}
+	//reset variables
+	head = NULL;
+	cursor = NULL;
+}
+
+template<typename DataType>
+bool List<DataType>::isEmpty() const
+{
+	//if head is null it returns true, else false
+	if (head == NULL) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+template<typename DataType>
+bool List<DataType>::isFull() const
+{
+	return false;
+}
+
+template<typename DataType>
+void List<DataType>::gotoBeginning() throw(logic_error)
+{
+	//check if the list is empty
+	if (head == NULL) {
+		throw logic_error("List is empty\n");
+	}
+	else{
+		//set the cursor to the first node
+		cursor = head;
+	}
+}
+
+template<typename DataType>
+void List<DataType>::gotoEnd() throw(logic_error)
+{
+	//check if the list is empty
+	if (head == NULL) {
+		throw logic_error("List is empty\n");
+	}
+	else {
+		//keep updating the cursor until it reaches the last node
+		while (cursor->next != 0) {
+			cursor = cursor->next;
+		}
+	}
+}
+
+template<typename DataType>
+bool List<DataType>::gotoNext() throw(logic_error)
+{
+	//throw error if list if empty
+	if (head == NULL) {
+		throw logic_error("List is empty\n");
+	}
+	//check if this is the last node in the list
+	else if (cursor->next == 0) {
+		return false;
+	}
+	else {
+		//move cursor to the next node
+		cursor = cursor->next;
+		return true;
+	}
+}
+
+template<typename DataType>
+bool List<DataType>::gotoPrior() throw(logic_error)
+{
+	//check if the list is empty
+	if (head == NULL) {
+		throw logic_error("List is empty\n");
+	}
+	//check if the cursor is pointing to the first node
+	else if(cursor == head)
+		return false;
+	else {
+		//create a temp variable and set that to head and go through the list until the next node is temp
+		ListNode *temp = cursor;
+		cursor = head;
+		while (cursor->next!= temp) {
+			cursor = cursor->next;
+		}
+	}
+
+}
+
+template<typename DataType>
+void List<DataType>::moveToBeginning() throw(logic_error)
+{
+	//if list is empty, throw logic error
+	if (isEmpty())
+		throw logic_error("list is empty\n");
+	else {
+		/*add current cursor data item to the new node, set the next pointer to head 
+		and set head to temp and remove current cursor and set cursor to head*/
+		ListNode *temp = new ListNode(cursor->dataItem, 0);
+		temp->next = head;
+		head = temp;
+		remove();
+		cursor = head;
+	}
+}
+
+template<typename DataType>
+void List<DataType>::insertBefore(const DataType & newDataItem) throw(logic_error)
+{
+	if (isFull())
+		throw logic_error("List is full\n");
+	else {
+		//sets the cursor to the beginning
+		gotoPrior();
+		//if the cursor is at the head
+		if (cursor == head) {
+			//add data item to the new node, set the next pointer to head and set head to temp and cursor to head
+			ListNode *temp = new ListNode(newDataItem, 0);
+			temp->next = head;
+			head = temp;
+			cursor = head;
+		}
+		else {
+			insert(newDataItem);
+		}
+	}
+}
+
+
+
+template<typename DataType>
+DataType List<DataType>::getCursor() const throw(logic_error)
+{
+	//check if the list is empty
+	if (head == NULL) {
+		throw logic_error("List is empty\n");
+	}
+	//return the dataItem at cursor
+	else {
+		return cursor->dataItem;
+	}
+}
+
+template<typename DataType>
+inline List<DataType>::ListNode::ListNode(const DataType & nodeData, ListNode * nextPtr)
+{
+	//set nodeData to dataItem and nextPtr to next
+	dataItem = nodeData;
+	next = nextPtr;
+}
+template <typename DataType>
+void List<DataType>::showStructure() const
+
+// Outputs the items in a list. If the list is empty, outputs
+// "Empty list". This operation is intended for testing and
+// debugging purposes only.
+
+{
+	if (isEmpty())
+	{
+		cout << "Empty list" << endl;
+	}
+	else
+	{
+		if (isEmpty())
+		{
+			cout << "Empty list" << endl;
+		}
+		else
+		{
+			for (ListNode* temp = head; temp != 0; temp = temp->next) {
+				if (temp == cursor) {
+					cout << "[";
+				}
+
+				// Assumes that dataItem can be printed via << because
+				// is is either primitive or operator<< is overloaded.
+				cout << temp->dataItem;
+
+				if (temp == cursor) {
+					cout << "]";
+				}
+				cout << " ";
+			}
+			cout << endl;
+		}
+	}
+}
+template<typename DataType>
+void List<DataType>::removeElement(int val)
+{
+	cursor = head;
+	while (cursor != NULL) {
+		int num = cursor->dataItem;
+		if (num == val) {
+			ListNode *p, *q;
+			//if cursor at the beginning, set p to head and change head and cursor to the next node and delete p
+			if (cursor == head) {
+				p = head;
+				head = cursor->next;
+				cursor = cursor->next;
+				delete p;
+			}
+			/*if the cursor is at the end, set p to cursor and cursor to head and go through
+			the list and set q to cursor and point q to NULL */
+			else if (cursor->next == 0) {
+				p = cursor;
+				cursor = head;
+				while (cursor->next != p) {
+					cursor = cursor->next;
+				}
+				q = cursor;
+				q->next = NULL;
+				delete p;
+				cursor = cursor->next;
+			}
+			/*if the cursor is somewhere in the middle of the list, set p to cursor and cursor to head
+			go through the list until it reaches the node before p, set q to that and set q pointer to p ponter
+			and cursor to p pointer.*/
+			else {
+				p = cursor;
+				cursor = head;
+				while (cursor->next != p) {
+					cursor = cursor->next;
+				}
+				q = cursor;
+				q->next = p->next;
+				cursor = p->next;
+				delete p;
+			}
+		}
+		else {
+			cursor = cursor->next;
+		}
+	}
+
+}
---
+++ Singly_Linked_List/slideshow.cpp
@@ -0,0 +1,152 @@
+//--------------------------------------------------------------------
+//
+//  Laboratory 5, Programming Exercise 1                 slideshow.cpp
+//
+//  (Shell) Slide show program
+// Name: Shivang Trivedi
+// Date: Feb 19, 2019
+// Lab: 05-exercise
+// Description: This program reads slides from a text file and prints it out on screen.
+//--------------------------------------------------------------------
+
+
+
+#include <ctime>
+#include <conio.h> // For function getch()
+#include <cstdlib> // For several general-purpose functions
+#include <fstream> // For file handling
+#include <iomanip> // For formatted output
+#include <iostream> // For cin, cout, and system
+#include <string> // For string data type
+#include <set>  //For sets
+
+using namespace std;
+
+#include "ListLinked.cpp"
+
+
+//--------------------------------------------------------------------
+
+// This is a very ugly way to wait for a specified amount of time.
+// It is ugly because it runs the CPU the whole time, instead of
+// just suspending the process for the required time period.
+// However, it is also very portable.  This function should work on
+// all platforms that support ANSI C++.  Feel free to replace this
+// with something nicer (such as sleep or usleep) if you so choose.
+void wait(int secs) 
+{
+    int start=clock();
+
+    while (clock() < start + secs * CLOCKS_PER_SEC);
+}
+
+//--------------------------------------------------------------------
+
+class Slide
+{
+  public:
+    static const int HEIGHT = 10,		// Slide dimensions
+                     WIDTH  = 36;
+
+    void display () const;			// Display slide and pause
+
+  private:
+
+    char image [HEIGHT][WIDTH];  	   // Slide image
+    int pause;				   // Seconds to pause after
+   					   //  displaying slide
+
+    friend istream& operator>> (istream& in, Slide& slide);
+    friend ostream& operator<< (ostream& out, const Slide& slide);
+};
+
+//--------------------------------------------------------------------
+
+int main()
+{
+	List<Slide> slideShow;    	    // Slide show
+	Slide currSlide;                // Slide
+	char filename[81];              // Name of slide show file
+
+	// Read the slide show from the specified file.
+
+	cout << endl << "Enter the name of the slide show file : ";
+	cin >> filename;
+
+	ifstream slideFile(filename);
+
+	if (!slideFile)
+	{
+		cout << "Error opening file " << filename << endl;
+	}
+	else
+	{
+		//read from file until end of file is reached
+		while (!slideFile.eof()) {
+				//get slide from file
+				slideFile >> currSlide;
+				//insert the current slide into the list
+				slideShow.insert(currSlide);
+				//print current slide
+				currSlide.display();
+		}
+
+		// Close the file. 
+		slideFile.close();
+		// Display the slide show slide-by-slide. (Your code below)
+
+	}
+    return 0;
+}
+
+//--------------------------------------------------------------------
+
+istream& operator>> (istream& inFile, Slide& slide)
+
+// Read a slide from inFile.
+{
+	inFile >> slide.pause;
+    // Read a slide from inFile.
+	for (int x = 0; x < slide.HEIGHT; x++) {
+		for (int y = 0; y < slide.WIDTH; y++) {
+			inFile.get(slide.image[x][y]);
+		}
+	}
+	return inFile;
+}
+
+//--------------------------------------------------------------------
+
+ostream& operator<< (ostream& out, const Slide& slide)
+
+{
+	//print the 2D array
+	for (int x = 0; x < slide.HEIGHT; x++) {
+		for (int y = 0; y < slide.WIDTH; y++) {
+			out << slide.image[x][y];
+		}
+	}
+	//return the ostream
+	return out;
+
+
+}
+
+
+void Slide:: display () const
+
+{
+    //This should compile and run on both Windows and Unix
+    //Feel free to change this if you want to do something nicer.
+    #ifdef WIN32
+        system("cls");
+    #else
+        system("clear");
+    #endif
+
+    // Display a slide and pause.
+		//output current slide. wait function for pause
+		cout << (*this);
+		wait(this->pause);
+
+}
